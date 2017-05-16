ff19bb3204f38cb39b75c14290737615f05d18d3
Maxwell Carlson <bart4282819@gmail.com>
2017-05-15 22:12:36 -0700

    ++
    counts words + number of times a entererd string occurs.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string.h>
+#include <valarray>
 
 using namespace std;
 
@@ -19,6 +24,57 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
+    
+    string tmp;
+    cout << "Please enter file name(in same directory as .exe)" << endl;
+    cin >> tmp;
+    
+    //create file object, read file object, check if null
+    //FILE* pFile;
+    //pFile = fopen(tmp.c_str(), "r");
+    
+    cout << "Enter string you'd like to search for" << endl;
+    string characters;
+    cin >> characters;
+    
+    ifstream file;
+    file.open(tmp.c_str());
+    
+    if(file == NULL){
+        cout << "Error with file" << endl;
+        exit(0);
+    } 
+    
+    
+    int numberT = 0, wordCount = 0;
+    //size_t pos{0};
+    
+    string word;
+    while(file >> word){
+        wordCount ++;
+        size_t pos {0};
+        //size_t found = word.find(characters);
+        //if(found != string::npos){
+        //    numberT++;
+        //}
+        size_t found = word.find(characters);
+        if(found != string::npos){
+            for(;;){
+                pos = word.find(characters,pos);
+                if(pos == string::npos){
+                    break;
+                }
+                ++ pos;
+                ++ numberT;        
+            }
+        }
+        
+    }
+    
+    cout << numberT << endl;
+    
+    
+    
 
     return 0;
 }
